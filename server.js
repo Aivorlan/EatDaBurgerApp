06513a11bc02977cd593d376fae573f8a1c03e9f
Aivorlan <aivorlan@icloud.com>
2020-12-31 16:04:42 -0600

    importing routes and handlebars

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 // Declaring variables that Require express and express-handlebars 
 var express = require("express");
-var exHandlebars = require("express-handlebars");
+// var override = require("method-override");
+var bodyParser = require("body-parser");
+
+var port = process.env.PORT || 8000;
+
+var app = express();
+ // Import routes and give the server access to them.
+var routes = require("./controllers/burger_controller.js");
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.use("/", routes);
+
+app.listen(port);
